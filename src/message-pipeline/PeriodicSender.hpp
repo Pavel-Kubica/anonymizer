4f09338db105422c4817461446c19fc6ceb585af
Pavel Kubica <kubicpa3@fit.cvut.cz>
2024-07-25 12:40:51 +0200

    Clarify interval of PeriodicSender

--- src/message-pipeline/PeriodicSender.hpp
+++ src/message-pipeline/PeriodicSender.hpp
@@ -9,7 +9,8 @@
 class PeriodicSender
 {
 public:
-    const inline static std::chrono::duration INTERVAL = std::chrono::seconds(63);
+    const inline static std::chrono::duration INTERVAL = std::chrono::seconds(60 + 3);
+    // with fewer than 3 seconds extra, sometimes the next request times out anyway
 
     PeriodicSender(AsyncFileManager* fileManager, DBManager* dbManager);
 
