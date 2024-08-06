f8bd84ecd71bfb9c6c0745fd9616b380bc8448e5
Pavel Kubica <kubicpa3@fit.cvut.cz>
2024-08-06 20:35:07 +0200

    Remove shutdown guard

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,13 @@
 
 #include "Application.hpp"
 #include <csignal>
-#include <functional>
 #include <iostream>
 
-
-std::function<void(int)> terminationHandler;
-void signalHandler(int signal)
-{
-    std::cout << "Shutdown initiated, please wait up to 1 minute." << std::endl;
-    terminationHandler(signal);
-    exit(0);
-}
 
 int main()
 {
     Application app;
     app.start();
-    terminationHandler = [&](int dummy) { app.stop(); };
-    signal(SIGINT, signalHandler);
     sleep (10000);
     return 0;
 }
